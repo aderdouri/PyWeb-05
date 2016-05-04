4ff3476f956a1ecb607bfb76a6ffd83b396c3f40
aderdouri <aderdouri@free.fr>
2016-05-04 21:56:30 +0200

    --

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -3,6 +3,8 @@ from sqlalchemy import (
     Index,
     Integer,
     Text,
+    DateTime,
+    func,
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -25,3 +27,21 @@ class MyModel(Base):
     value = Column(Integer)
 
 Index('my_index', MyModel.name, unique=True, mysql_length=255)
+
+class Entry(Base):
+    __tablename__ = 'entries'
+    id = Column(Integer, primary_key=True)
+    title = Column(Text)
+    body = Column(Text, nullable=True)
+    created = Column(DateTime, default=func.now())
+    edited = Column(DateTime, default=func.now())
+
+    @classmethod
+    def all(cls, session):
+        return session.query(cls).order_by(cls.id.desc()).all()
+
+    @classmethod
+    def by_id(cls, session, myid):
+        return session.query(cls).filter(cls.id==myid).first()
+
+Index('Entry_index', Entry.title, unique=True, mysql_length=255)
---
+++ learning_journal/mytests_pshell.py
@@ -0,0 +1,59 @@
+"""
+unit Tests using pshell
+"""
+from sqlalchemy import engine_from_config
+engine = engine_from_config(registry.settings, 'sqlalchemy.')
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker(bind=engine)
+session = Session()
+from learning_journal.models import Entry
+session.query(Entry).all()
+
+query = session.query(Entry)
+type(query)
+
+q1 = session.query(Entry)
+for row in q1:
+    print(row)
+    print(type(row))
+
+q2 = session.query(Entry.id, Entry.title, Entry.body, Entry.created, Entry.edited)
+for id, title, body, created, edited in q2:
+	print(id)	
+	print(type(id))
+	print(title)
+	print(type(title))
+	print(body)
+	print(type(body))
+	print(created)
+	print(type(created))
+	print(edited)
+	print(type(edited))
+
+session.query(Entry).get(1)
+session.query(Entry).get(10)
+
+new_model = Entry(id=1, title='first example', body='todo')
+session.add(new_model)
+new_model = Entry(id=2, title='second example', body='toto')
+session.add(new_model)
+session.commit()
+
+session = Session()
+session.query(Entry).count()
+
+"""
+Print all
+"""
+[(model.id, model.title, model.body, model.created, model.edited) for model in session.query(Entry)]
+
+session.query(Entry).order_by(Entry.id.desc()).all()
+
+"""
+test classmethod all and by_id
+"""
+Entry.all(session)
+Entry.by_id(session, 1)
+
+[(model.id, model.title, model.body, model.created, model.edited) for model in Entry.all(session)]
+
